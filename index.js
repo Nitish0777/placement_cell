697448072cc520ffbd9f1945c73f1cb089066461
Nitish Nashine <nitishnashine@gmail.com>
2023-08-08 09:24:51 +0530

    connection made to mongodb

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import routes from "./routes/index.js";
+import colors from "colors";
+import db from "./config/mongoose.js";
 
 const app = express();
 
 //configure Env
 dotenv.config();
+db();
 
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
@@ -22,5 +27,5 @@ app.listen(port, function (err) {
   if (err) {
     console.log(`Error in Running server ${err}`);
   }
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`.bgCyan.white);
 });
